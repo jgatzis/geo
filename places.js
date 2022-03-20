e3ff0069f64e6ee90016458b8ca9fa587cc25458
jgatzis <76046200+jgatzis@users.noreply.github.com>
2022-03-20 18:30:05 +0200

    Update places.js

--- places.js
+++ places.js
@@ -73,7 +73,7 @@ window.onload = () => {
                     icon.setAttribute('src', './map-marker.png');
 
                     // for debug purposes, just show in a bigger scale, otherwise I have to personally go on places...
-                    icon.setAttribute('scale', '20, 20');
+                    icon.setAttribute('scale', '10, 10');
 
                     icon.addEventListener('loaded', () => window.dispatchEvent(new CustomEvent('gps-entity-place-loaded')));
 
