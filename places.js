9c4f302346707cf13d18a4e37c92e41200e6c94b
jgatzis <76046200+jgatzis@users.noreply.github.com>
2022-03-21 12:03:36 +0200

    Update places.js

--- places.js
+++ places.js
@@ -75,10 +75,11 @@ window.onload = () => {
                     const longitude = place.location.lng;
 
                     
-                    const entity = document.createElement('a-entity');
+                    const entity = document.createElement('a-text');
                    <!-- entity.setAttribute('src', 'https://jgatzis.github.io/ar/assets/photos/1.png'); -->
-                    entity.setAttribute('text', 'test');
+                    entity.setAttribute('value', 'test');
                     entity.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
+                     entity.setAttribute('scale', '100, 100');
                       scene.appendChild(entity);
                     // add place icon
                     const icon = document.createElement('a-image');
