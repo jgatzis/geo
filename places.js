0ad1128e5ec4de23f08f9ae48fd9b17bc3fb9d80
jgatzis <76046200+jgatzis@users.noreply.github.com>
2022-03-22 10:28:17 +0200

    Update places.js

--- places.js
+++ places.js
@@ -41,6 +41,7 @@ window.onload = () => {
                 places.forEach((place) => {
                     const latitude = place.location.lat;
                     const longitude = place.location.lng;
+                    const srcImg = place.src
                    
                   
                     // add place icon
@@ -70,7 +71,7 @@ window.onload = () => {
                             container.appendChild(label);
                            document.body.appendChild(container);
                            
-                            window.open('https://jgatzis.github.io/ar/assets/photos/1.png');
+                            window.open(srcImg);
                        //     const img = document.createElement('span');
                          //     const container2 = document.createElement('div');
                           //    container2.setAttribute('id', 'img');
